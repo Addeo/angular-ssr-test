911b6eda5e6b015317bcbec1c39e94c887cc8e5f
Sergey <sergey.cosilov@gmail.com>
2023-11-13 13:59:10 +0300

    init ssr

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Untitled3
+# WebOboronaAdmin
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.0.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiAlertModule,
+  TUI_SANITIZER,
+  TuiTextfieldControllerModule, TuiButtonModule
+} from '@taiga-ui/core';
 
 import { AppComponent } from './app.component';
+import {TuiInputModule} from "@taiga-ui/kit";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet,
+    HttpClientModule,
+    BrowserAnimationsModule, // Required by Taiga UI
+    TuiRootModule, // Has to go after BrowserAnimationsModule
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiInputModule,
+    TuiTextfieldControllerModule,
+    ReactiveFormsModule,
+    TuiButtonModule
   ],
-  providers: [],
+  providers: [ provideClientHydration(), {provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
